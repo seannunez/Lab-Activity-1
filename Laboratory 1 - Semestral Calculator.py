a09e741b69a22477e33ec5a2cde4def5c9411a6f
Sean Nuñez <71063880+seannunez@users.noreply.github.com>
2021-03-10 22:19:19 +0800

    Update Laboratory 1 - Semestral Calculator.py

--- Laboratory 1 - Semestral Calculator.py
+++ Laboratory 1 - Semestral Calculator.py
@@ -1,42 +1,33 @@
 def calculator():
     global result
 
-    Fullname = input("Full Name: ")
-    student_number = input("Student#: ")
-    course = input("Course: ")
-    prelims = input("Prelim: ")
-    midterms = input("Midterm: ")
-    finals = input("Finals: ")
+    Fullname        = input("Full Name: ")
+    student_number  = input("Student#: ")
+    course          = input("Course: ")
+    prelims         = input("Prelim: ")
+    midterms        = input("Midterm: ")
+    finals          = input("Finals: ")
 
-    prelim, midterm, final = 0.3, 0.3, 0.4
-    Final_average = prelim + midterm + final
-    Final_grade = round(Final_average, 2)
+    Final_average = (int(prelims) + int(midterms) + int(finals)) / 3
     Happy = "\U0001F600"
     Laugh = "\U0001F606"
-    Sad = "\U0001F62D"
-    if Final_average > 70:
+    Sad   = "\U0001F62D"
+    if   Final_average > 70:
         result = Happy
     elif Final_average == 70:
         result = Laugh
     elif Final_average < 70:
         result = Sad
 
-    print(("\nFull name: " + Fullname) +
-          ("\nStudent#: " + student_number) +
-          ("\nCourse: " + course) +
-          ("\nPrelim Grade: " + prelims) +
-          ("\nMidterm Grade: " + midterms) +
-          ("\nFinal Grade: " + finals) +
-          ("\nFinal Average Grade on this course: " + str(Final_grade)) +
-          ("\nResult: " + result) +
-           "\n------------------------------------------------------")
+    print("Hello {},{}!!! your semestral grade in {} is: {:.2f} ".format(Fullname, student_number, course, Final_average))
     calculateagain()
+
 
 def calculateagain():
     print("")
     answer = input("DO YOU WANT TO TRY AGAIN?:Y/N ")
     print("")
-    if answer == "Y" or answer == "y":
+    if   answer == "Y" or answer == "y":
         calculator()
     elif answer == "N" or answer == "n":
         quit()
